beff55d5354d7bf366827b83828e0a733fc2703f
Alan Sousa <alan.stark2@gmail.com>
2022-05-08 18:06:14 -0300

    segundo commit

---
+++ 04-conditionais.php
@@ -0,0 +1,15 @@
+<?php 
+
+/*
+ < Less than
+ > Greater than
+ <= Less than or equal to
+ >= Greater than or equal to
+ == Equal to
+ === Identical to
+ != Not equal to
+ !== No identical to
+*/
+
+ 
+?>
